19928c7a0bda2ec811d28bcf3287280557d45fa6
BaglioniChristian <chrysbaglioni@gmail.com>
2025-11-03 18:30:42 +0100

    MainFile.java

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,77 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static String primo="";
+    static String primo = "";
+    static FileWriter file; // writer globale in stile C
+
     public static void main(String[] args) {
+        try {
+            file = new FileWriter("gara_cavalli.txt"); // apertura file
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         Scanner input = new Scanner(System.in);
         String tmpS;
         int tmp;
-        ArrayList<Cavallo> listaCavallo = new ArrayList<Cavallo>();
+        ArrayList<Cavallo> listaCavallo = new ArrayList<>();
+
         for (int i = 1; i <= 4; i++) {
             System.out.println("Inserisci il nome del cavallo " + i);
-            tmpS =  input.nextLine();
+            tmpS = input.nextLine();
             System.out.println("Inserisci la lentezza del cavallo " + i);
             tmp = input.nextInt();
-            String v = input.nextLine(); //prende il \n
-            Cavallo c=new Cavallo(tmpS, tmp);
+            input.nextLine(); // consuma il \n
+            Cavallo c = new Cavallo(tmpS, tmp, file); // passiamo writer
             listaCavallo.add(c);
         }
-        for(Cavallo c: listaCavallo){
+
+        for (Cavallo c : listaCavallo) {
             c.start();
         }
-        for(Cavallo c: listaCavallo){
+
+        Random r = new Random();
+        int indexAzzoppato = r.nextInt(listaCavallo.size());
+        Cavallo cavalloAzzoppato = listaCavallo.get(indexAzzoppato);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            file.write(" Il cavallo " + cavalloAzzoppato.getNameCavallo() + " è stato azzoppato!\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        cavalloAzzoppato.interrupt();
+
+        for (Cavallo c : listaCavallo) {
             try {
                 c.join();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-        System.out.println("Il primo cavallo: " + primo);    }
+
+        try {
+            file.write("Il primo cavallo: " + primo + "\n");
+            file.close(); // chiusura file
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static String getPrimo() {
         return primo;
     }
+
     public static void setPrimo(String primo) {
         Main.primo = primo;
     }
