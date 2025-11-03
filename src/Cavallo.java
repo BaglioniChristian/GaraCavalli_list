d53073ef6ce0f30ac91184e3323551a99e94d849
BaglioniChristian <chrysbaglioni@gmail.com>
2025-11-03 18:29:27 +0100

    CavalloFile.java

--- src/Cavallo.java
+++ src/Cavallo.java
@@ -1,35 +1,59 @@
-public class Cavallo extends Thread{
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Cavallo extends Thread {
 
     private final String name;
     private int lentezza;
-    public Cavallo(String name, int lentezza) {
+    private FileWriter file;
+
+    public Cavallo(String name, int lentezza, FileWriter file) {
         super();
         this.name = name;
         this.lentezza = lentezza;
+        this.file = file;
     }
-    /**override del metodo run */
+
     @Override
-    public void run(){
-        System.out.println("Cavallo " +
-                name + " comincia il suo galoppo");
+    public void run() {
+        try {
+            file.write("Cavallo " + name + " comincia il suo galoppo\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         for (int i = 1; i <= 10; i++) {
             try {
                 sleep(lentezza);
             } catch (InterruptedException e) {
+                try {
+                    file.write(" Cavallo " + name + " è stato azzoppato e si ferma!\n");
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                return;
+            }
+            try {
+                file.write(name + " cavalca - passo: " + i + "\n");
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println(name +" cavalca - passo: " + i);
         }
-        if(Main.getPrimo().equals("")){
+
+        if (Main.getPrimo().equals("")) {
             Main.setPrimo(this.name);
         }
     }
-    /**getter per il parametro di lentezza SUPERFLUI*/
-    protected int getLentezza(){
+
+    protected int getLentezza() {
         return lentezza;
     }
-    /**setter per il parametro di lentezza */
-    protected void setLentezza(int lentezza){
-        this.lentezza=lentezza;
+
+    protected void setLentezza(int lentezza) {
+        this.lentezza = lentezza;
+    }
+
+    protected String getNameCavallo() {
+        return name;
     }
 }
